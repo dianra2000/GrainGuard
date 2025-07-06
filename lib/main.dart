efff43c1f6f15907c1b401aa7f58517a8e05087a
dianra2000 <dihanianudiranathunga@gmail.com>
2025-07-06 14:57:10 +0530

    some features updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'auth/login_screen.dart';
 import 'home_screen.dart';
+import 'firebase_options.dart'; // Make sure this import exists
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -24,8 +28,8 @@ class MyApp extends StatelessWidget {
         inputDecorationTheme: InputDecorationTheme(
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8),
+          ),
         ),
-      ),
       ),
       home: const LoginScreen(),
       routes: {
